0752ed376dd51c2eabff479ea73ed2cc27225806
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-04-14 08:08:06 +0530

    first commit

---
+++ dsa.js
@@ -0,0 +1,23 @@
+// 1. . Write a function called countOccurrences()
+// that takes in a string and a character and returns
+// the number of occurrences of that character in the string.
+
+// const countOccurrences = (str, char) => {
+//   let count = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// };
+
+// const result = countOccurrences("bananA", "a");
+// console.log(result);
+
+// 2. Write a function called findMaxNumber that takes
+// in an array of numbers and returns the largest number
+// in the array.
+
