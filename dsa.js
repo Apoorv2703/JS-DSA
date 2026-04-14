4e34f30766600d220a7251c654b52b164c11cbc6
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-04-14 08:24:28 +0530

    second commit

--- dsa.js
+++ dsa.js
@@ -21,3 +21,20 @@
 // in an array of numbers and returns the largest number
 // in the array.
 
+let findMaxNumber = (arr)=>{
+
+    let max = arr[0]
+
+    for(let i = 0 ; i<arr.length ; i++){
+        if(arr[i] > max){
+            max = arr[i]
+        }
+    }
+
+    return max
+
+}
+
+let result = findMaxNumber([1,2,3,4,5,22,6,7,8])
+console.log(result);
+
