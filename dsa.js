78c34a4d04f94f32e952d6b65a273d9fa7b2ad1d
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-04-22 08:28:29 +0530

    22 apr

--- dsa.js
+++ dsa.js
@@ -264,6 +264,23 @@
 // }
 // console.log(count);
 
+//10. Find sum of array elements
+
+let arr = [1,2,3,4]
+
+let sum = 0 
+
+for(let i = 0 ; i < arr.length ; i++){
+    sum = sum + arr[i]
+}
+
+console.log(sum);
+
+
+
+
+
+
 
 
 
