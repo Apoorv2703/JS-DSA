92116c90fa53e6fe6cb7f26c396bf9b087b1a78b
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-04-28 20:01:25 +0530

    last digit

--- dsa.js
+++ dsa.js
@@ -296,13 +296,7 @@
 //input = 1234
 //output = 4321 
 
-// let n = 1234 
-// let reverse = 0 
 
-// for(; n > 0 ; n = parseInt(n/10)){
-//     reverse =  n % 10
-//     console.log(reverse);
-// }
 
 //sum of all digits 
 //input = 1234 
@@ -318,21 +312,19 @@
 
 // console.log(sum); 
 
-// Reverse a number
+// Print Last Digit of a Number
 
-// 👉 Input: 1234 → Output: 4321
+// 👉 Input: 987 → Output: 7
 
-// let n = 1234 
+let n  = 987 
 
-// let reverse = 0
+let lastDigit = n % 10 
+console.log(lastDigit);
 
-// for(; n > 0 ; n = parseInt(n / 10)){
-//   let digit = n % 10 
-//   reverse = reverse * 10 + digit
-    
-    
-// }
-// console.log(reverse);
+
+
+
+
 
 
 
