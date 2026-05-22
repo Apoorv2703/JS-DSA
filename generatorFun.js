2c89864c080629eab86e62fc81fd95b8b050b384
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-05-22 16:04:21 +0530

    genfun

--- generatorFun.js
+++ generatorFun.js
@@ -9,3 +9,5 @@ let result = genFun()
 console.log(result.next())
 console.log(result.next())
 console.log(result.next("i am apoorv"))
+
+//normal function can return only once but generator function can return (yield) more than 1
