afcea50ccf5628c3e3b01266fc031cad272814bf
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-05-22 16:07:06 +0530

    genfun

--- generatorFun.js
+++ generatorFun.js
@@ -1,13 +1,30 @@
-function* genFun(){
-    yield "apoorv"
-    let a = yield "patel"
-    return a
-}
+// function* genFun(){
+//     yield "apoorv"
+//     let a = yield "patel"
+//     return a
+// }
 
-let result = genFun()
+// let result = genFun()
 
-console.log(result.next())
-console.log(result.next())
-console.log(result.next("i am apoorv"))
+// console.log(result.next())
+// console.log(result.next())
+// console.log(result.next("i am apoorv"))
 
 //normal function can return only once but generator function can return (yield) more than 1
+
+
+function* genFunc(){
+    yield console.log('this is 1st line')
+    yield console.log('this is 2nd line')
+    yield console.log('this is 3rd line')
+    yield console.log('this is 4th line')
+}
+
+let generator1 = genFunc()
+
+console.log(generator1.next())
+//ans this is 1st line
+// { value: undefined, done: false } same on next 3 as well
+console.log(generator1.next())
+console.log(generator1.next())
+console.log(generator1.next())
