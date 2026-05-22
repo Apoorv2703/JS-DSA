ee6b68f5bd6be518bfbfee354bf85f4c02a46d5d
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-05-22 16:16:28 +0530

    genfun

--- generatorFun.js
+++ generatorFun.js
@@ -13,18 +13,34 @@
 //normal function can return only once but generator function can return (yield) more than 1
 
 
-function* genFunc(){
-    yield console.log('this is 1st line')
-    yield console.log('this is 2nd line')
-    yield console.log('this is 3rd line')
-    yield console.log('this is 4th line')
-}
+// function* genFunc(){
+//     yield console.log('this is 1st line')
+//     yield console.log('this is 2nd line')
+//     yield console.log('this is 3rd line')
+//     yield console.log('this is 4th line')
+// }
 
-let generator1 = genFunc()
+// let generator1 = genFunc()
 
-console.log(generator1.next())
+// console.log(generator1.next())
 //ans this is 1st line
 // { value: undefined, done: false } same on next 3 as well
-console.log(generator1.next())
-console.log(generator1.next())
-console.log(generator1.next())
+// console.log(generator1.next())
+// console.log(generator1.next())
+// console.log(generator1.next())
+
+
+
+function* genFunc(){
+    yield ('this is 1st line')
+    yield ('this is 2nd line')
+    yield ('this is 3rd line')
+    yield ('this is 4th line')
+}
+
+let neww = genFunc()
+
+console.log(neww.next().value);
+// ans = this is 1st line
+
+
