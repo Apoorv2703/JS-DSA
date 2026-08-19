848efc786592cb23aeee95d53e2a1c1c922c35b5
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-08-19 22:36:44 +0530

    array question

--- array.js
+++ array.js
@@ -1,14 +1,26 @@
-let arr = [5,8,2,10,3]
-log(arr.length)
+// let arr = [5,8,2,10,3]
+// console.log(arr.length);
 
-for(let i = 0 ; i < arr.length ; i++){
-    console.log(arr[i]);
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(arr[i]);
     
-}
+// }
 
 //if you console i only it will give you index like 0 1 2 3 4 
 // but if you console arr[i] it will give you the value stored in index 
 // let i = 0 represents the index
+
+
+let arr = [5 , 8, 2,10,3 ]
+
+let count = 0
+
+for(let i = 0 ; i < arr.length ; i++){
+    count = count + arr[i]
+}
+
+console.log(count);
+
 
 
 
