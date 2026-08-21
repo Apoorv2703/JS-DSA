63f2cc419fabafb7abc14c1e1dfd51397e0a7e8f
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-08-21 21:54:49 +0530

    smallest in array

--- array.js
+++ array.js
@@ -34,17 +34,33 @@
 
 // console.log(largest); 
 
- let arr = [-10, -5, -20, -3, -15];
+//  let arr = [-10, -5, -20, -3, -15];
 
- let largets  = arr[0]
+//  let largets  = arr[0]
 
- for(let i = 0 ; i < arr.length ; i++){
-    if(largets < arr[i]){
-        largets = arr[i]
-    }
- }
+//  for(let i = 0 ; i < arr.length ; i++){
+//     if(largets < arr[i]){
+//         largets = arr[i]
+//     }
+//  }
 
- console.log(largets);
+//  console.log(largets);
+
+
+// let arr = [7, 3, 9, 2, 8, 1]; 
+
+// let smallest = arr[0]
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     if(arr[i] < smallest){
+//         smallest = arr[i]
+//     }
+// }
+
+// console.log(smallest);
+
+
+
  
 
 
